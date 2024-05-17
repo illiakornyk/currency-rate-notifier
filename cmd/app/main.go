5daf0a5290bdad356e8a7d042420a83ba695bb7f
Illia Kornyk <krekerbrow@gmail.com>
2024-05-17 15:19:10 +0300

    feat: make call to exchange rates api

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,11 +4,58 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/url"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/illiakornyk/currency-rate-notifier/internal/app/exchange_rates"
+	"github.com/illiakornyk/currency-rate-notifier/internal/app/utils"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	    // Load the .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+
+ 	  // Retrieve the API key from the environment variable
+    apiKey := os.Getenv("EXCHANGERATESAPI_KEY")
+
+     // Construct the base URL
+    baseURL := "http://api.exchangeratesapi.io/v1/latest"
+
+    // Create a URL object from the base URL
+    u, err := url.Parse(baseURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Prepare query parameters
+    q := u.Query()
+    q.Set("access_key", apiKey)
+    q.Set("symbols", "USD,UAH")
+    q.Set("format", "1")
+
+    // Assign encoded query parameters to the URL object
+    u.RawQuery = q.Encode()
+
+    // The fully constructed URL with the embedded API key and other parameters
+    apiURL := u.String()
+    exchangeRatesData, err := exchange_rates.FetchExchangeRates(apiURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	    // Use the exchangeRates data as needed
+    // For example, convert EUR to USD and UAH to USD
+    usdToUahRate := utils.ConvertEURtoUSDUAH(exchangeRatesData.Rates["USD"], exchangeRatesData.Rates["UAH"])
+
+    // Output the rate for verification
+    log.Printf("USD to UAH rate: %f", usdToUahRate)
+
     // Construct the DSN from the environment variables or directly.
 	dsn := "user:password@tcp(localhost:3306)/mydb"
 
