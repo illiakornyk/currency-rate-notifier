c917bdc29b6bb2202581db6e4e716e5b6b5c478e
Illia Kornyk <krekerbrow@gmail.com>
2024-05-18 18:37:37 +0300

    refactor: split up rates handler on separated functions

--- internal/app/handlers/rates.go
+++ internal/app/handlers/rates.go
@@ -8,46 +8,50 @@ import (
 
 	"github.com/illiakornyk/currency-rate-notifier/internal/app/config"
 	"github.com/illiakornyk/currency-rate-notifier/internal/app/exchange_rates"
+	"github.com/illiakornyk/currency-rate-notifier/internal/app/utils"
 )
 
-// RatesHandler handles requests for the /rates route
-func RatesHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a URL object from the base URL
+// ConstructAPIURL constructs the URL to fetch exchange rates.
+func ConstructAPIURL() (string, error) {
 	u, err := url.Parse(config.BaseURL)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	// Prepare query parameters
 	q := u.Query()
 	q.Set("access_key", config.APIKey)
 	q.Set("symbols", "USD,UAH")
 	q.Set("format", "1")
 
-	// Assign encoded query parameters to the URL object
 	u.RawQuery = q.Encode()
 
-	// The fully constructed URL with the embedded API key and other parameters
-	apiURL := u.String()
+	return u.String(), nil
+}
 
-	// Fetch the exchange rates data
+// FetchAndRespondExchangeRate fetches the exchange rates and writes the response.
+func FetchAndRespondExchangeRate(w http.ResponseWriter, apiURL string) {
 	exchangeRatesData, err := exchange_rates.FetchExchangeRates(apiURL)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// Extract the USD to UAH rate
-	usdToUahRate := exchangeRatesData.Rates["UAH"]
+	usdToUahRate := utils.ConvertEURtoUSDUAH(exchangeRatesData.Rates["USD"], exchangeRatesData.Rates["UAH"])
 
-	// Create a struct to format the response
 	type response struct {
 		Rate float64 `json:"rate"`
 	}
 
-	// Set the Content-Type header
 	w.Header().Set("Content-Type", "application/json")
-
-	// Write the response as JSON
 	json.NewEncoder(w).Encode(response{Rate: usdToUahRate})
+}
+
+// RatesHandler handles requests for the /rates route.
+func RatesHandler(w http.ResponseWriter, r *http.Request) {
+	apiURL, err := ConstructAPIURL()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	FetchAndRespondExchangeRate(w, apiURL)
 }
