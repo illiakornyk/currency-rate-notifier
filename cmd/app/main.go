c9ba0dd572be8608f6010915bca5a22ed1d3076e
Illia Kornyk <krekerbrow@gmail.com>
2024-05-18 13:21:08 +0300

    feat: insert email in db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/illiakornyk/currency-rate-notifier/internal/app/config"
 	"github.com/illiakornyk/currency-rate-notifier/internal/app/handlers"
+	"github.com/illiakornyk/currency-rate-notifier/internal/app/subscription"
 )
 
 
@@ -16,6 +18,13 @@ func main() {
 	config.Init()
 
 	http.HandleFunc("/rate", handlers.RatesHandler)
+
+	err := subscription.InsertEmail("myMail@example.com")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Email inserted successfully!")
 
 	fmt.Println("Server is running on port 8080...")
 	http.ListenAndServe(":8080", nil)
--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -12,7 +12,7 @@ import (
 var (
 	APIKey  string
 	BaseURL string
-	db *sql.DB
+	DB *sql.DB
 
 )
 
@@ -44,21 +44,21 @@ func Init() {
 
 func initDB() {
 	// Retrieve database credentials from environment variables
-	dbUser := os.Getenv("MYSQL_USER")
-	dbPassword := os.Getenv("MYSQL_PASSWORD")
-	dbHost := "localhost"
+	dbUser := "root"
+	dbPassword := os.Getenv("MYSQL_ROOT_PASSWORD")
+	dbHost := os.Getenv("MYSQL_HOST")
 	dbName := os.Getenv("MYSQL_DATABASE")
 
 	// Construct the DSN using the environment variables
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUser, dbPassword, dbHost, dbName)
 	var err error
-	db, err = sql.Open("mysql", dsn)
+	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
 
 	// Check if the connection is successful.
-	err = db.Ping()
+	err = DB.Ping()
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
