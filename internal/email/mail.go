b68fbd31156c0a999c0c8b79b4e85fbe8784c5e6
Illia Kornyk <forest.bukson@gmail.com>
2024-06-04 00:01:45 +0300

    refactor: change to private functions

--- internal/email/mail.go
+++ internal/email/mail.go
@@ -12,12 +12,12 @@ type SMTPServer struct {
 }
 
 // ServerName URI to smtp server
-func (s *SMTPServer) ServerName() string {
+func (s *SMTPServer) serverName() string {
 	return s.Host + ":" + s.Port
 }
 
 // BuildMessage creates the message to be sent
-func BuildMessage(from, to, subject, body string) []byte {
+func buildMessage(from, to, subject, body string) []byte {
 	headers := "From: " + from + "\r\n" +
 		"To: " + to + "\r\n" +
 		"Subject: " + subject + "\r\n\r\n"
@@ -32,13 +32,13 @@ func SendEmail(from, password, to, subject, body string) error {
 
 	smtpServer := SMTPServer{Host: smtpHost, Port: smtpPort}
 
-	message := BuildMessage(from, to, subject, body)
+	message := buildMessage(from, to, subject, body)
 
 	// Authentication.
 	auth := smtp.PlainAuth("", from, password, smtpServer.Host)
 
 	// Sending email.
-	err := smtp.SendMail(smtpServer.ServerName(), auth, from, []string{to}, message)
+	err := smtp.SendMail(smtpServer.serverName(), auth, from, []string{to}, message)
 	if err != nil {
 		return err
 	}
