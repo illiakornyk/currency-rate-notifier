ca1574346a72a475349f13e9ae3a9abb0a39ea24
Illia Kornyk <krekerbrow@gmail.com>
2024-06-05 16:38:31 +0300

    refactor: rid of unsued function

--- internal/app/utils/exchange_rates.go
+++ internal/app/utils/exchange_rates.go
@@ -1,6 +1,1 @@
 package utils
-
-// ConvertEURtoUSD,UAH takes the EUR to USD and EUR to UAH rates and converts them to a USD to UAH rate
-func ConvertEURtoUSDUAH(eurToUSD, eurToUAH float64) float64 {
-    return eurToUAH / eurToUSD
-}
