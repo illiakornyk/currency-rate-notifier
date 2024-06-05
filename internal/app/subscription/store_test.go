199e68b98ce05eaa0e90fcc434931785792e140e
Anastasya Golubenko <golubenkoanastasya@gmail.com>
2024-06-05 16:20:07 +0300

    test: add test for fetching email

--- internal/app/subscription/store_test.go
+++ internal/app/subscription/store_test.go
@@ -29,3 +29,36 @@ func TestAddEmail(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
+
+
+func TestFetchEmails(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	config.DB = db
+
+	rows := sqlmock.NewRows([]string{"email"}).
+		AddRow("user1@example.com").
+		AddRow("user2@example.com")
+
+	mock.ExpectQuery("SELECT email FROM emails").WillReturnRows(rows)
+
+	emails, err := fetchEmails()
+	if err != nil {
+		t.Errorf("error was not expected while fetching emails: %s", err)
+	}
+
+	expectedEmails := []string{"user1@example.com", "user2@example.com"}
+	for i, email := range emails {
+		if email != expectedEmails[i] {
+			t.Errorf("expected email %v, got %v", expectedEmails[i], email)
+		}
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
