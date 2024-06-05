42dc301e3b434ec68268a62489b2b621acdd2bf6
Anastasya Golubenko <golubenkoanastasya@gmail.com>
2024-06-05 15:05:52 +0300

    refactor: rid of unnecessary field

--- internal/app/models/exchange_rates.go
+++ internal/app/models/exchange_rates.go
@@ -2,7 +2,7 @@ package models
 
 // ExchangeRatesResponse represents the response structure from the exchange rates API
 type CurrencyInfo struct {
-	R030         int     `json:"r030"`
+	// R030         int     `json:"r030"`
 	Txt          string  `json:"txt"`
 	Rate         float64 `json:"rate"`
 	Cc           string  `json:"cc"`
